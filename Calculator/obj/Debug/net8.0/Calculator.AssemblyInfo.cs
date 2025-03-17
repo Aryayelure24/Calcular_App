8bb78db957290033cb0a51834294b9b2eba95a32
Arya Harshavardhan Yelure <arya.yelure@zensar.com>
2025-03-18 02:58:28 +0530

    Added sealed Calcultor class with EvaluateExpression method.

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d941f703b653d4888c7d8c6d7eacf04365216e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7dd734fd24b9da35f10163ad3c94aa257e301b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
