b089b7f8b51201ea1e1e475ca46bf65fa89ecb01
Arya Harshavardhan Yelure <arya.yelure@zensar.com>
2025-03-17 15:11:33 +0530

    Implemented Addition,Division,Multiplication,Substraction and created interace IOperation

---
+++ Calculator/Operations/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Operations
+{
+    public interface IOperation
+    {
+        public double Execute(double a, double b);
+        public double Execute(double a);
+    }
+}
